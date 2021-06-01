e0d39bafefd90b6951eb447108f5ffb0aa2b35bb
josh <jo3f@mtmail.mtsu.edu>
2021-06-01 13:24:26 -0500

    check

--- old/kernel/sys.h
+++
@@ -1,9 +0,0 @@
-#ifndef OS_SYS_H
-#define OS_SYS_H
-
-#include "sys/idt.h"
-#include "sys/isr.h"
-#include "sys/x86asm.h"
-#include "sys/utils.h"
-
-#endif
--- old/kernel/sys/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef OS_UTILS_H
-#define OS_UTILS_H
-
-#include <stdint.h>
-
-void memcopy(unsigned char *dest, unsigned char *src, int bytes);
-void memset(uint8_t *dest, uint8_t val, uint32_t len);
-
-#endif
